4c69bacd4ebe3e5edef42e316d21ddf568f50c4d
yimeantw <yimeantw@yahoo.com.tw>
2018-05-24 17:31:21 +0800

    Add files via upload
    This application needs to execute with SettingInfo.txt; and you need to create by your self.
    The format of content of SettingInfo.txt is ID;PWD;maximum waiting time;target url.

--- CheckinAndCheckout.py
+++ CheckinAndCheckout.py
@@ -1,13 +1,20 @@
 '''
 Release note:
     Author:brook.huang@fengsui.com.tw
-    ver.1&2 relese date:20180516
-Ver.1 This application is used to auto check in and check out.
-Ver.2 Add dandom delay funtion and intorduce ID and password from file.
+
+20180516. This application is used to auto check in and check out.
+          Add dandom delay funtion and intorduce ID and password from file.
+20180524. add IAP[0]=ID
+          IAP[1]=PWD
+          IAP[2]=random maximum value
+          IAP[3]=url
 Note:
     20480516 currently, this applicatio needs to company with windows' task
     schedular to execute. There is one thing shoudl be mentioned, the
-    chromedriver.exe should be placed in the same folder.    
+    chromedriver.exe should be placed in the same folder. 
+  
+
+ 
 '''
 
 
@@ -17,7 +24,7 @@ from selenium import webdriver
 #from selenium.webdriver.common.action_chains import ActionChains
 #ActionChains is used to perform right click
 import random
-file_path=os.path.abspath('IDAndPWD.txt')
+file_path=os.path.abspath('SettingInfo.txt')
 print(file_path)
 
 try:
@@ -26,12 +33,12 @@ except FileNotFoundError:
     print("File Not Found")
 finally:    
     IAP=input_file.read().split(';')
-    print(IAP[0],IAP[1]) #used to debug
-    x=random.randint(0,100)
+    print(IAP[0],IAP[1],IAP[2],IAP[3]) #used to debug  
+    x=random.randint(0,int(IAP[2]))
     print(x)
     time.sleep(x)
     driver = webdriver.Chrome()
-    driver.get("https://pro.104.com.tw/hrm/psc/home.action")    
+    driver.get(IAP[3])    
     driver.find_element_by_id("email").send_keys(str(IAP[0]))
     driver.find_element_by_id("pwd").send_keys(str(IAP[1]))
     before_login_url=driver.current_url #used to debug
@@ -42,7 +49,7 @@ if before_login_url == after_login_url: #used to debug
     print("unchanged") #used to debug
 else: #used to debug
     print("changed") #used to debug
-    driver.get("https://pro.104.com.tw/hrm/psc/home.action") 
+    driver.get(IAP[3]) 
     redirection_url=driver.current_url
     print(redirection_url)
 time.sleep(10)
